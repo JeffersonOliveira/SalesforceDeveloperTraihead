b8492059d6388a32e7fb68135aaeb72aea4c66c6
Jefferson Nunes S. de Oliveira <jefferson_nunes01@hotmail.com>
2022-04-28 22:34:19 -0300

    Improving a Lwc with some buttons

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,5 +2,10 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     name = 'Jefferson';
+    clickedButtonLabel;
 
+    handleClick(event) {
+        this.clickedButtonLabel = event.target.label;
+    }
 }
+
